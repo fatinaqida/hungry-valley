3c6f3bfe9b1eb7d0eddc583f7912db87f886c9c4
Emira Y <emirahanna@gmail.com>
2024-03-16 14:53:23 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import './src/counter-app.js';
 
 function App() {
   return (
     
     <div className="App">
-      <counter-app></counter-app>
       <header className="App-header">
         <img src="https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/duw9vgampaecvtjfytjo.png" />
         <p>
